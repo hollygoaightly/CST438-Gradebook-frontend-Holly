d6baf5caef15f51299d20ce585ccb07ad737df4f
Holly <hstephens@csumb.edu>
2021-10-12 21:35:21 -0700

    troubleshooting

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'https://cst438gradebook-backend.herokuapp.com/';
+export const SERVER_URL = 'http://localhost:8081';
+//export const SERVER_URL = 'https://cst438gradebook-backend.herokuapp.com/';
